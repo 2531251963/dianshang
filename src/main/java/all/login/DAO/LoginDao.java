602888e4a837eb2e96ce4d015fea7b4a0904ff6c
zxmic <2537011619@qq.com>
2020-01-09 18:30:37 +0800

    zx login

---
+++ src/main/java/all/login/DAO/LoginDao.java
@@ -0,0 +1,35 @@
+package all.login.DAO;
+
+import all.login.Entity.User;
+import all.util.JdbcUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LoginDao {
+    //根据电话号码 查询用户名帐号
+    public User selectUserByP(Statement stmt,ResultSet rs,String phonenumber){
+
+        User user=new User();
+        String sql="select (userid,phonenumber,password) from user where phonenumber ='"+phonenumber+"'";
+        try {
+            rs=stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            //System.out.println("selectUserByP");
+            e.printStackTrace();
+        }
+        try {
+            if(rs.next()){
+                user.setId(rs.getInt("id"));
+                user.setPhoneNumber(rs.getString("phonenumber"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            System.out.println("没有搜索到user信息");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
