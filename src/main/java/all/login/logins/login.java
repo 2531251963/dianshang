0a107745f0d610c6fb6f3d6471b90ef9ea6e57ec
zx <2537011619@qq.com>
2020-01-26 20:40:34 +0800

    zx

--- src/main/java/all/login/logins/login.java
+++ src/main/java/all/login/logins/login.java
@@ -4,21 +4,18 @@ package all.login.logins;
 import all.login.Entity.User;
 import all.login.Service.LoginService;
 import all.login.Service.SerializeObjectTool;
-import all.util.JdbcUtil;
 import all.util.RedisUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import redis.clients.jedis.Jedis;
-
-import java.sql.Connection;
 import java.util.Map;
 
 public class login {
-    private Jedis redis;
+    private Jedis jedis;
 
     public login(){
-        redis = RedisUtil.getJedis();
-        redis.auth("123456");
+        jedis = RedisUtil.getJedis();
+        jedis.auth("123456");
     }
     public String login(String data){
 
@@ -27,7 +24,35 @@ public class login {
 
         //前端登录界面传进json
         //1、在redis中的hashmap查询k是否存在
-        byte[] byt = redis.get("hashmap".getBytes());
+        String str1=lookForInHashmap(user);
+        if(str1.equals("Login_OK")){
+            return str1;
+        }
+
+        //2、在redis中查询缓存是否存在
+        String str2=lookForInKV(user);
+        if(str2.equals("Login_OK")){
+            return str2;
+        }
+
+        //3、查询数据库
+        String str3=lookForInMysql(user);
+        if(str3.equals("Login_OK")){
+            return str3;
+        }else if(str3.equals("Login")){
+            //三步查询没有找到，数据库存在号码信息，但是密码出错，显示登录页面，重新登录；
+            return "Login";
+        }else {
+            //三步查询没有找到，并且数据库不存在号码信息，显示注册页面
+            return "Register";
+        }
+
+    }
+
+
+    //1、在redis中的hashmap查询k是否存在
+    public String lookForInHashmap(User user){
+        byte[] byt = jedis.get("hashmap".getBytes());
         try{
             Map<String, String> result = SerializeObjectTool.unserizlizeMap(byt);
             int count=0;
@@ -44,13 +69,17 @@ public class login {
             }
             if(count==0){
                 System.out.println("hashmap缓存中没有找到user.getphonrnumber");
+                return "Login_False";
             }
         }catch (NullPointerException e){
             System.out.println("缓存中没有hashmap 空指针");
         }
+        return "Login_False";
+    }
 
-        //2、在redis中查询缓存是否存在
-        byte[] byt1 = redis.get(("p"+user.getPhoneNumber()).getBytes());
+    //2、在redis中查询缓存是否存在
+    public String lookForInKV(User user){
+        byte[] byt1 = jedis.get(("p"+user.getPhoneNumber()).getBytes());
         try {
             Object obj = SerializeObjectTool.unserizlize(byt1);
             String str = (String)obj;
@@ -63,15 +92,22 @@ public class login {
         }catch (NullPointerException e){
             System.out.println("缓存中没有找到user.getphonrnumber");
         }
+        return "Login_False";
+    }
 
-        //3、查询数据库
+    //3、查询数据库
+    public String lookForInMysql(User user){
         LoginService loginService = new LoginService();
-        User user1=loginService.selectUserByP(user.getPhoneNumber());
-        if(user1!=null){
-            return "Loin_OK";
+        User userMysql=loginService.selectUserByPhoneNumber(user.getPhoneNumber());
+        if(userMysql!=null){
+            if(userMysql.getPassword().equals(user.getPassword())){
+                return "Login_OK";
+            }else {
+                return "Login";
+            }
         }else {
             System.out.println("数据库中没有找到phonenumber，请先注册");
-            return "Login_FAILE";
+            return "Register";
         }
     }
 }
