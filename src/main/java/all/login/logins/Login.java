61472f5632c6b60a71be4f59704e0ea94367dec8
zx <2537011619@qq.com>
2020-01-31 14:09:00 +0800

    zx

--- src/main/java/all/login/logins/Login.java
+++ src/main/java/all/login/logins/Login.java
@@ -5,6 +5,7 @@ import all.login.entity.User;
 import all.login.Service.LoginService;
 import all.util.RedisUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import redis.clients.jedis.Jedis;
 
@@ -24,73 +25,86 @@ public class Login {
     public String loginM(String data){
         //data中含有phoneNumber和password
         JSONObject json = new JSONObject();
-        JSONObject jsonObject = JSONObject.parseObject(data);
-        User user = JSON.toJavaObject(jsonObject,User.class );
-        //字符替换
-        //user.setPhoneNumber(user.getPhoneNumber());
+        User user=null;
+        try{
+            JSONObject jsonObject = JSONObject.parseObject(data);
+            user = JSON.toJavaObject(jsonObject,User.class );
+        }catch (JSONException e){
+            json.put("msg","Attack!");
+            return json.toString();
+        }
 
-        //前端登录界面传进json
-        //1、在redis中的hashmap查询phoneNumber是否存在，找到对应userid，且判断redis密码是否正确
-        int i1=lookForInHashmap(user);
-        switch (i1) {
-            //3、该用户没有注册过，跳转到注册界面。
-            case 4:
-                json.put("userid",userid);
-                json.put("token",token);
-                json.put("msg", "Not_Register");
-                return json.toString();
-            //4、用户名密码正确
-            case 1:
-                //把正确账号密码保存token
-                token = Token.saveTokenToRedis(jedis,userid);
-                json.put("userid",userid);
-                json.put("token", token);
-                json.put("msg", "Success_Login");
-                return json.toString();
-            //5、redis中密码错误，重新登录
-            case 2:
-                json.put("userid",userid);
-                json.put("token", token);
-                json.put("msg", "WrongPassword");
-                return json.toString();
 
-            //6、用户注册过，但是redis中不存在账号密码，查询数据库
-            case 3:
-                int i2 = lookForInMysql(user);
-                switch (i2) {
-                    //7、数据库查询账号密码正确
-                    case 11:
-                        //把正确账号密码保存token
-                        token = Token.saveTokenToRedis(jedis,userid);
-                        //把正确账号密码保存redis
-                        saveRecordToRedis(user);
-                        json.put("userid",userid);
-                        json.put("token", token);
-                        json.put("msg", "Success_Login");
-                        return json.toString();
-                    //8、数据库查询密码错误
-                    case 12:
-                        json.put("userid",userid);
-                        json.put("token",token);
-                        json.put("msg", "WrongPassword");
-                        return json.toString();
-                    //9、数据库没有查询到信息，显示注册
-                    case 13:
-                        json.put("userid",userid);
-                        json.put("token",token);
-                        json.put("msg", "Not_Register");
-                        return json.toString();
-                    default:
-                        json.put("userid",userid);
-                        json.put("token",token);
-                        json.put("msg", "Wrong_Login");
-                        return json.toString();
-                }
-            default:
-                json.put("userid",userid);
-                json.put("token",token);
-                json.put("msg", "Wrong_Login");
-                return json.toString();
+//        //字符替换
+//        user.setPhoneNumber(user.getPhoneNumber().replaceAll("'",""));
+
+        if(!user.getPhoneNumber().equals("phoneNumber")&&!user.getPassword().equals("password")){
+            //前端登录界面传进json
+            //1、在redis中的hashmap查询phoneNumber是否存在，找到对应userid，且判断redis密码是否正确
+            int i1=lookForInHashmap(user);
+            switch (i1) {
+                //3、该用户没有注册过，跳转到注册界面。
+                case 4:
+//                    json.put("userid",userid);
+//                    json.put("token",token);
+                    json.put("msg", "Not_Register");
+                    return json.toString();
+                //4、用户名密码正确
+                case 1:
+                    //把正确账号密码保存token
+                    token = Token.saveTokenToRedis(jedis,userid);
+                    json.put("userid",userid);
+                    json.put("token", token);
+                    json.put("msg", "Success_Login");
+                    return json.toString();
+                //5、redis中密码错误，重新登录
+                case 2:
+                    json.put("userid",userid);
+                    //json.put("token", token);
+                    json.put("msg", "WrongPassword");
+                    return json.toString();
+
+                //6、用户注册过，但是redis中不存在账号密码，查询数据库
+                case 3:
+                    int i2 = lookForInMysql(user);
+                    switch (i2) {
+                        //7、数据库查询账号密码正确
+                        case 11:
+                            //把正确账号密码保存token
+                            token = Token.saveTokenToRedis(jedis,userid);
+                            //把正确账号密码保存redis
+                            saveRecordToRedis(user);
+                            json.put("userid",userid);
+                            json.put("token", token);
+                            json.put("msg", "Success_Login");
+                            return json.toString();
+                        //8、数据库查询密码错误
+                        case 12:
+                            json.put("userid",userid);
+                            //json.put("token",token);
+                            json.put("msg", "WrongPassword");
+                            return json.toString();
+                        //9、数据库没有查询到信息，显示注册
+                        case 13:
+//                            json.put("userid",userid);
+//                            json.put("token",token);
+                            json.put("msg", "Not_Register");
+                            return json.toString();
+                        default:
+//                            json.put("userid",userid);
+//                            json.put("token",token);
+                            json.put("msg", "Wrong_Login");
+                            return json.toString();
+                    }
+                default:
+//                    json.put("userid",userid);
+//                    json.put("token",token);
+                    json.put("msg", "Wrong_Login");
+                    return json.toString();
+            }
+        }else {
+            json.put("msg","Lack_Info");
+            return json.toString();
         }
     }
 
